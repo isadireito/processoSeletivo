7b8971c83ca634fab4670186cb441e7ae5f838c4
Isabella Direito <isa.martinsj3@gmail.com>
2026-03-08 18:45:03 -0300

    red: testes canCreateUffMail falham pois metodo nao existe

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -2,8 +2,20 @@ package processoSeletivo.test;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import src.Student;  
 
 public class StudentTest {
 
+    @Test
+    void trueIfStudentsIsOn() {
+        Student student = new Student("Laura", "105457", "9999", "laura@gmail.com", null, "Ativo");
+        assertTrue(student.canCreateUffMail());
+    }
+
+    @Test
+    void cantCreateUffMail() {
+        Student student = new Student("Joao", "123", "999", "joao@gmail.com", null, "Inativo");
+        assertFalse(student.canCreateUffMail());
+    }
 }
+
