bda6e2f374f1ee1c88ffb9ac65b39f449c08ea26
Isabella Direito <isa.martinsj3@gmail.com>
2026-03-08 18:40:42 -0300

    estrutura inicial

---
+++ src/Student.java
@@ -0,0 +1,71 @@
+package src;
+
+
+public class Student {
+    private String name;
+    private String studentId;
+    private String phone;
+    private String email;
+    private String uffMail;
+    private String status;
+
+    public Student(String name, String studentId, String phone, String email, String uffMail, String status){
+        this.name = name;
+        this.studentId = studentId;
+        this.phone = phone;
+        this.email = email;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUffMail() {
+        return uffMail;
+    }
+
+    public void setUffMail(String uffMail) {
+        this.uffMail = uffMail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+
+
+}
